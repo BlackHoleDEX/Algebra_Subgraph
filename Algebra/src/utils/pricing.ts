34c26d16fbe45bc3344f9488d74de209ab686b01
IliaAzhel <aje-ilia2013@yandex.by>
2024-11-20 13:32:03 +0300

    arb config

--- Algebra/src/utils/pricing.ts
+++ Algebra/src/utils/pricing.ts
@@ -4,16 +4,16 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WMatic_ADDRESS = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
-const USDC_WMatic_03_POOL = '0xb104f0535a35a69880dab51008756c31d47dbf0f'
+const WMatic_ADDRESS = '0x82af49447d8a07e3bd95bd0d56f35241523fbab1'
+const USDC_WMatic_03_POOL = '0xfd615c7fff517526733837442ef72e8e1147ba4c'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // WMATIC
-  '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
-  '0x5aefba317baba46eaf98fd6f381d07673bca6467', // USDT 
-  '0x49a390a3dfd2d01389f799965f3af5961f87d228'
+  '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', // WETH
+  '0xaf88d065e77c8cc2239327c5edb3a432268e5831', // USDC
+  '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', // USDC(arb)
+  '0x3d9907f9a368ad0a51be60f7da3b97cf940982d8' // GRAIL
 ]
 
 let MINIMUM_Matic_LOCKED = BigDecimal.fromString('0')
@@ -21,8 +21,8 @@ let MINIMUM_Matic_LOCKED = BigDecimal.fromString('0')
 let Q192 = Math.pow(2, 192)
 
 let STABLE_COINS: string[] = [
-  '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
-  '0x5aefba317baba46eaf98fd6f381d07673bca6467' // SUDT
+  '0xaf88d065e77c8cc2239327c5edb3a432268e5831', // USDC
+  '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8' // USDC(arb)
 ]
 
 
@@ -41,7 +41,7 @@ export function priceToTokenPrices(price: BigInt, token0: Token, token1: Token):
 export function getEthPriceInUSD(): BigDecimal {
   let usdcPool = Pool.load(USDC_WMatic_03_POOL) // dai is token0
   if (usdcPool !== null) {
-    return usdcPool.token0Price
+    return usdcPool.token1Price
   } else {
     return ZERO_BD
   }
