61d981213da8831fc09b2e474a279a17839eacc3
IliaAzhel <aje-ilia2013@yandex.by>
2024-12-18 13:56:11 +0300

    bitfinity config

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xDfCC62B3eE40EE453Cb2788576B56e746FC79827'
+export const FACTORY_ADDRESS = '0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
--- Algebra/src/utils/pricing.ts
+++ Algebra/src/utils/pricing.ts
@@ -4,14 +4,14 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WMatic_ADDRESS = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
+const WMatic_ADDRESS = '0x10253594a832f967994b44f33411940533302acb'
 const USDC_WMatic_03_POOL = '0xb104f0535a35a69880dab51008756c31d47dbf0f'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // WMATIC
-  '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
+  '0x10253594a832f967994b44f33411940533302acb', // WMATIC
+  '0x83d4a9ea77a4dba073cd90b30410ac9f95f93e7c', // USDC
   '0x5aefba317baba46eaf98fd6f381d07673bca6467', // USDT 
   '0x49a390a3dfd2d01389f799965f3af5961f87d228'
 ]
@@ -22,7 +22,7 @@ let Q192 = Math.pow(2, 192)
 
 let STABLE_COINS: string[] = [
   '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
-  '0x5aefba317baba46eaf98fd6f381d07673bca6467' // SUDT
+  '0x83d4a9ea77a4dba073cd90b30410ac9f95f93e7c' // SUDT
 ]
 
 
