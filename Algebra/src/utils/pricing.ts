584c58e707a8a278c8d529cf758964e94c04f449
IliaAzhel <aje-ilia2013@yandex.by>
2025-04-02 15:53:02 +0300

    change usd address

--- Algebra/src/utils/pricing.ts
+++ Algebra/src/utils/pricing.ts
@@ -5,13 +5,13 @@ import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
 const WMatic_ADDRESS = '0x4eb2bd7bee16f38b1f4a0a5796fffd028b6040e9'
-const USDC_WMatic_03_POOL = '0xb4641c5919e4b2f985ed7c40f0ac400332aea31a'
+const USDC_WMatic_03_POOL = '0x6953a8e22de899f815b1a3b56d286ef340ea0d35'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
   '0x4eb2bd7bee16f38b1f4a0a5796fffd028b6040e9', // WMATIC
-  '0xb83b20806524e435f517e8d6ee2d4c519d439d2f', // USDC
+  '0x38a5c36fa8c8c9e4649b51fcd61810b14e7ce047', // USDC
   '0x5aefba317baba46eaf98fd6f381d07673bca6467', // USDT 
   '0x49a390a3dfd2d01389f799965f3af5961f87d228'
 ]
@@ -21,7 +21,7 @@ let MINIMUM_Matic_LOCKED = BigDecimal.fromString('0')
 let Q192 = Math.pow(2, 192)
 
 let STABLE_COINS: string[] = [
-  '0xb83b20806524e435f517e8d6ee2d4c519d439d2f' // USDC
+  '0x38a5c36fa8c8c9e4649b51fcd61810b14e7ce047' // USDC
 ]
 
 
@@ -40,7 +40,7 @@ export function priceToTokenPrices(price: BigInt, token0: Token, token1: Token):
 export function getEthPriceInUSD(): BigDecimal {
   let usdcPool = Pool.load(USDC_WMatic_03_POOL) // dai is token0
   if (usdcPool !== null) {
-    return usdcPool.token1Price
+    return usdcPool.token0Price
   } else {
     return ZERO_BD
   }
