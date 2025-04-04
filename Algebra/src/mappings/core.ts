d623b2e8ee2c69f50641457be7a3602cc7fb93b2
IliaAzhel <aje-ilia2013@yandex.by>
2025-04-04 11:58:26 +0300

    Revert "sentio config"
    This reverts commit 052a0d9dababc1b81d37f126333998eaa2d74d8a.

--- Algebra/src/mappings/core.ts
+++ Algebra/src/mappings/core.ts
@@ -692,11 +692,9 @@ export function handleChangeFee(event: ChangeFee): void {
 }
 
 export function handlePlugin(event: PluginEvent): void {
-  let pool = Pool.load(event.address.toHexString())
-  if (pool != null) {
-    pool.plugin = event.params.newPluginAddress
-    pool.save()
-  }
+  let pool = Pool.load(event.address.toHexString())!
+  pool.plugin = event.params.newPluginAddress
+  pool.save()
 
   let plugin = Plugin.load(event.params.newPluginAddress.toHexString())
   if (plugin === null) {
