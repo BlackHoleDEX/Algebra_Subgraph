d53092c3819d96fe8fa76bf80fac6ba575b9a696
IliaAzhel <aje-ilia2013@yandex.by>
2025-04-14 16:58:39 +0300

    update static definition token list

--- Algebra/src/utils/staticTokenDefinition.ts
+++ Algebra/src/utils/staticTokenDefinition.ts
@@ -11,10 +11,40 @@ export class StaticTokenDefinition {
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
     const staticDefinitions: Array<StaticTokenDefinition> = [
       {
-        address: Address.fromString('0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'),
-        symbol: 'DGD',
-        name: 'DGD',
+        address: Address.fromString('0x10253594A832f967994b44f33411940533302ACb'),
+        symbol: 'WTAC',
+        name: 'Wrapped TAC',
+        decimals: BigInt.fromI32(18)
+      },
+      {
+        address: Address.fromString('0x83D4a9Ea77a4dbA073cD90b30410Ac9F95F93E7C'),
+        symbol: 'USDC',
+        name: 'USDC',
+        decimals: BigInt.fromI32(18)
+      },
+      {
+        address: Address.fromString('0xbE3C16e14d578a24eF4B124fAf9CD1bb5F1e964B'),
+        symbol: 'TON',
+        name: 'TON Token',
         decimals: BigInt.fromI32(9)
+      },
+      {
+        address: Address.fromString('0xc208e122Ff9915747eee968926be395eB5D9155C'),
+        symbol: 'X',
+        name: 'X Empire',
+        decimals: BigInt.fromI32(9)
+      },
+      {
+        address: Address.fromString('0x10253594A832f967994b44f33411940533302ACb'),
+        symbol: 'WTAC',
+        name: 'Wrapped TAC',
+        decimals: BigInt.fromI32(18)
+      },
+      {
+        address: Address.fromString('0x7336A5a3251b9259DDf8B9D02a96dA0153e0799d'),
+        symbol: 'USDT',
+        name: 'USDT',
+        decimals: BigInt.fromI32(18)
       },
     ]
     return staticDefinitions
