76bb98d7c26c84e89e3b5ff432719f451ead5ed2
Sriharsha <sriharsha@useblitz.com>
2025-06-03 15:08:35 +0530

    changes for backend deployment

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A'
+export const FACTORY_ADDRESS = '0xAA8CD6190C27A4f985dF0AE24DdA9A4a265700BD'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
