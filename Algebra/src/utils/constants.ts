08ffa8828fb68c87ff2f23b8350f4c7f35e1c63b
Sriharsha <sriharsha@useblitz.com>
2025-06-30 15:04:58 +0530

    updated code for testnet 30.6.2025

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xDA9Eaa81d5712A2643F936758eddc8C3322b6789'
+export const FACTORY_ADDRESS = '0xDC41bA754Fd6E524dD7B195B557434760F4952a7'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
