2bc311b41b8d8fe58ef241afde74c9185322a10d
Sriharsha <sriharsha@useblitz.com>
2025-07-02 18:40:51 +0530

    mockmainnet 1.0.1 deployment

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xDC41bA754Fd6E524dD7B195B557434760F4952a7'
+export const FACTORY_ADDRESS = '0x44Ee3D2ec3C86F21c0C187c937375D3c20479388'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
